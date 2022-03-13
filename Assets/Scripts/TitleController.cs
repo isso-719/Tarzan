ec5eb013f9b254852ca8c9ae57fa0c5509358718
isso-719 <i@kazukiisogai.net>
2022-03-13 18:23:00 +0900

    add title

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    public GameObject clickToPlayText;
+    public GameObject stageSelectButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && clickToPlayText.activeSelf)
+        {
+            clickToPlayText.SetActive(false);
+            stageSelectButtons.SetActive(true);
+        }
+    }
+
+    public void SelectStage(int stageNum)
+    {
+        SceneManager.LoadScene("Stage" + stageNum);
+    }
+}
